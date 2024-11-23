c1e6bead16f7e8b41f1c9a3b263c20960684c646
JoaoPe22 <joao.canezin22@gmail.com>
2024-11-23 06:34:06 -0400

    commit PWA-VITE
    Tem que verificar o cache

---
+++ vite-project/js/main.js
@@ -0,0 +1,14 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      let reg;
+      reg = await navigator.serviceWorker.register("/sw.js", {
+        type: "module",
+      });
+
+      console.log("Service worker registrada!", reg);
+    } catch (err) {
+      console.log("Service worker registro falhor: ", err);
+    }
+  });
+}
---
+++ vite-project/sw.js
@@ -0,0 +1,56 @@
+import { offlineFallback, warmStrategyCache } from "workbox-recipes";
+import { CacheFirst, StaleWhileRevalidate } from "workbox-strategies";
+import { registerRoute, Route } from "workbox-routing";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+import { CacheExpiration, ExpirationPlugin } from "workbox-expiration";
+
+const pageCache = new CacheFirst({
+  cacheName: "primeira-pwa-cache",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ["/index.html", "/", "/main.js", "/offline"],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
+
+registerRoute(
+  ({ request }) => ["style", "script", "worken"].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+offlineFallback({
+  pageFallback: "/offline.html",
+});
+
+const imageRoute = new Route(
+  ({ request }) => {
+    return request.destination === "image";
+  },
+  new CacheFirst({
+    cacheName: "images",
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 30,
+      }),
+    ],
+  })
+);
+
+registerRoute(imageRoute);
